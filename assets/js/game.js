939668e58347dae89a46c3b2e647599da83537af
Emily <emilyadarr@gmail.com>
2021-12-09 15:17:55 -0500

    randomize fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,17 +26,26 @@ var fightOrSkip = function() {
 };
 // this creates a function with parameter for enemy's name
 var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
   // repeat and execute as long as the enemy-robot is alive
   while(playerInfo.health > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or skip using fightOrSkip function
-    if (fightOrSkip()) {
-      // if true, leave fight by breaking loop
-      break;
-    }
+    if (isPlayerTurn) {
+      // ask player if they'd like to fight or skip using fightOrSkip function
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
     
     // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
+    // remove enemy's health
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
       playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
@@ -54,7 +63,9 @@ var fight = function(enemy) {
     } else {
       window.alert(enemy.name + " still has " + enemy.health + " health left.");
     }
-
+    // player gets attacked first
+  } else {
+    
     // remove player's health by subtracting the amount set in the enemy.attack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
@@ -72,6 +83,9 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
     }
   }    
+  // switch turn order for next round
+  isPlayerTurn = !isPlayerTurn;
+  }
 };
 
 // function to start a new game
